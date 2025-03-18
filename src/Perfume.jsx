777ec9e37ea86b0011f782a21b72e2475be31f99
hrm <hrm.2003@gmail.com>
2025-03-19 01:14:11 +0330

    fix bog

--- src/Perfume.jsx
+++ src/Perfume.jsx
@@ -1,44 +1,44 @@
+import { useParams, useNavigate, useLocation } from "react-router-dom";  
+import { getperfume, deleteperfume } from "./data/data";  
+import  { useState, useEffect } from "react";  
 
-import { useParams, useNavigate, useLocation } from "react-router-dom";
-import { getperfume, deleteperfume } from "./data/data";
-import React from "react";
+const Perfume = () => {  
+  const params = useParams();  
+  let location = useLocation();  
+  let navigate = useNavigate();  
+  const [perfume, setPerfume] = useState(null);  
 
-const Perfume = () => {
-  const params = useParams();
-  const perfume = getperfume(params.perfumeId);
-  let location = useLocation()
-  let navigate = useNavigate()
+  useEffect(() => {  
+    const fetchedPerfume = getperfume(params.perfumeId);  
+    setPerfume(fetchedPerfume);  
+  }, [params.perfumeId]);  
 
-  if (perfume) {
-    return (
-      <React.Fragment>
-        <div className="box-products">
-          <img src={perfume.img} alt="" width={"230px"} />
-          <div>
-
-            <label className="specifications" htmlFor="">Product specifications</label>
-          <ul className="list-detail">
-            <li>{perfume.name}</li>
-            <li>{perfume.price}</li>
-            <li>{perfume.author}</li>
-            <li>{perfume.description}</li>
-          </ul>
-          <button
-            onClick={() => {
-              deleteperfume(perfume.id);
-              navigate("/perfume" + location.search)
-            }}
-          >
-            Delet item
-          </button>
-          </div>
-
-        </div>
-      </React.Fragment>
-    );
-  } else {
-    return <div>Not found!!</div>;
-  }
-};
+   
+  if (perfume) {  
+    return (  
+      <div className="box-products">  
+        <img src={perfume.img} alt={perfume.name} width={"230px"} />  
+        <div>  
+          <label className="specifications" htmlFor="">Product specifications</label>  
+          <ul className="list-detail">  
+            <li>{perfume.name}</li>  
+            <li>{perfume.price}</li>  
+            <li>{perfume.author}</li>  
+            <li>{perfume.description}</li>  
+          </ul>  
+          <button onClick={() =>{
+            deleteperfume(perfume.id);  
+    
+            navigate("/perfumes" + location.search); 
+          }}>  
+            Delete item  
+          </button>  
+        </div>  
+      </div>  
+    );  
+  } else {  
+    return <div style={{color:"red" , fontSize:"2rem"}}>Not found!!</div>;  
+  }  
+};  
 
 export default Perfume;
